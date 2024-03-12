83ba1b36c9a78ecdecca02620d96cf1fd9e4e55b
hiracansu <hiracansudegirmenbasi@gmail.com>
2024-03-12 05:19:47 +0300

    taking html files and find text

--- src/ContentFetcher.py
+++ src/ContentFetcher.py
@@ -1,71 +1,57 @@
 from selenium import webdriver
 from typing import Union, Dict
-import threading
 from Logger import Logger
 import os
 import io
-#dict_items([('09167a15-92e3-4fa8-8e04-78a98e6416d3', 'https://www.google.com.tr/'), ('f2298a78-d1a8-4870-a70f-7f45509d61ba', 'https://www.google.com.tr/'), ('79448917-5a97-43fc-9e0d-0a9043d0f922', 'https://ubs.etu.edu.tr/'), ('f04e67bd-4845-4f88-8584-adb6d7e4da61', 'https://store.epicgames.com/'), ('d469e004-402a-430a-8777-56d445dde49d', 'about:newtab')])
+import shutil
+from TextExtractor import TextExtractor
 
 class ContentFetcher:
     def __init__(self, driver: Union[webdriver.Edge, webdriver.Chrome, webdriver.Firefox]):
         self.__driver = driver
-        self.__visitedTabUrlPair : Dict[str, str] = {}
-        self.reset()
-        self.__thread = threading.Thread(target=self.__process, args=())
-        self.__sensorThread = threading.Thread(target=self.__handleHTMLchanges, args=())
-
-    def reset(self):
-        self.__visitedTabUrlPair = {}
-        
-    def fetchAndPrintHtmlContents(self, pair: Dict[str, str]):
-        for tab_id, url in pair.items():
-            if tab_id not in self.__visitedTabUrlPair:
-                self.__visitedTabUrlPair[tab_id] = url
-                #Logger.warn(f"YENİ TAB {tab_id} ve {url}")
-                self.__htmlContentForTab(tab_id, url, True)
-            else:
-                if url != self.__visitedTabUrlPair[tab_id]:
-                    self.__visitedTabUrlPair[tab_id] = url
-                    #Logger.warn(f"YENİ URL tab {tab_id} ve {url}")
-                    self.__htmlContentForTab(tab_id, url, True)
-                else:
-                    pass
-
-        tabs_to_delete = [tab_id for tab_id in self.__visitedTabUrlPair if tab_id not in pair]
-
-        for tab_id in tabs_to_delete:
-            #Logger.warn(f"Bu {tab_id} ve {self.__visitedTabUrlPair[tab_id]} silindi ")
-            del self.__visitedTabUrlPair[tab_id]
-            self.__htmlContentForTab(tab_id, url, False)
+        self.html_files_directory = "html_files"
+        self.text_files_directory = "text_files"
+        self.__reset_directories()
+        self.textExtractor = TextExtractor(self.text_files_directory)
     
-    def __htmlContentForTab(self, tab_id: str, url: str, isOpen: bool):
-        directory = r"C:\\bitirme_496\\src\\html_files"
-        if not os.path.exists(directory):
+    def __reset_directories(self):
+        for directory in [self.html_files_directory, self.text_files_directory]:
+            if os.path.exists(directory):
+                shutil.rmtree(directory)
             os.makedirs(directory)
-        
-        file_path = os.path.join(directory, f"{tab_id}.txt")
-        
-        if isOpen:
-            self.__driver.get(url)
-            html_content = self.__driver.page_source
+
+    def fetchAndPrintHtmlContents(self, handlesDict, currentHandle):
+        if currentHandle in handlesDict:
+            url = handlesDict[currentHandle]
+            current_html_content = self.__driver.page_source
+            html_file_path = os.path.join(self.html_files_directory, f"{currentHandle}.txt")
             
-            with io.open(file_path, 'w', encoding='utf-8') as file:
-                file.write(html_content)
-                # Logger.warn(f"HTML content for tab {tab_id} has been saved to {file_path}")
+            shouldExtractText = False
+            if os.path.exists(html_file_path):
+                with open(html_file_path, "r", encoding="utf-8") as file:
+                    existing_html_content = file.read()
+                if existing_html_content == current_html_content:
+                    #Logger.warn(f"No changes detected in {url}. File {html_file_path} remains unchanged.")
+                    return
+                else:
+                    shouldExtractText = True
+            else:
+                shouldExtractText = True
+
+            if shouldExtractText:
+                with open(html_file_path, "w", encoding="utf-8") as file:
+                    file.write(current_html_content)
+                Logger.warn(f"HTML content for {url} has been updated or saved for the first time to {html_file_path}.")
+                self.textExtractor.extractAndSaveText(current_html_content, currentHandle)
         else:
+            Logger.warn("Current handle is not found in handles dictionary.")
+
+    def deleteFiles(self, deletedTabHandle):
+        html_file_path = os.path.join(self.html_files_directory, f"{deletedTabHandle}.txt")
+        text_file_path = os.path.join(self.text_files_directory, f"{deletedTabHandle}.txt")
+        for file_path in [html_file_path, text_file_path]:
             if os.path.exists(file_path):
                 os.remove(file_path)
-
-        
-    def __handleHTMLchanges(self) -> None:
-        pass
-
-    def __process(self) -> None:
-        self.__sensorThread.start()
-        self.__sensorThread.join()
-    
-    def start(self) -> None:
-        self.__thread.start()
-
-    def join(self) -> None:
-        self.__thread.join()
+                Logger.warn(f"Deleted file for closed tab {deletedTabHandle}")
+            else:
+                Logger.warn(f"File for deleted tab does not exist: {file_path}")
--- src/URLTracker.py
+++ src/URLTracker.py
@@ -9,36 +9,26 @@ from Logger import Logger
 import threading
 from ContentFetcher import ContentFetcher     
 from selenium.common.exceptions import NoSuchWindowException    
-'''
-url değişikliğini bulmak
-tab acilip kapandiysa bunu fark edip onlari listeye eklemek cikarmak
-html contentleri alirken refresh kontrol et
-html'leri al
-'''
 
 class URLTracker:
     def __init__(self, driver):
         self.__driver = driver
-        self.__handles: List[str] = driver.window_handles.copy()
+        self.__handles = {}  
+        self.__handles.update({handle: self.__driver.current_url for handle in driver.window_handles})
         self._handleCount = len(self.__handles)
         self.__updateHandlesAndUrls()
         self.__contentFetcher = ContentFetcher(self.__driver)
-        self.__contentFetcher.reset()
         self.__lastActiveHandle = ""
-        #self._thread = threading.Thread(target=self._process, args=())
-        #self._refreshThread = threading.Thread(target=self._handleRefreshs, args=())
 
-        
 
     def trackUrls(self):
         try:
             currentHandles = self.__driver.window_handles
             original_handle = self.__driver.current_window_handle
-            #print(original_handle)
+
             if set(currentHandles) != set(self.__handles.keys()):            
                 self.__updateHandlesAndUrls(currentHandles)
             else:
-
                 if original_handle != self.__lastActiveHandle:
                     Logger.warn(f"Kullanici {self.__lastActiveHandle} sekmesinden {original_handle} sekmesine gecti")
                     self.__lastActiveHandle = original_handle
@@ -50,124 +40,56 @@ class URLTracker:
                     self.__handles[original_handle] = current_url
         except NoSuchWindowException:
             #Logger.error("Bir pencere/sekmeye erişilemiyor.")
-            self.__updateHandlesAndUrls()
+            self.__handleWindowClosedScenario()
         self.__trackHtmlContentsOfUrls()
     
     
     def __updateHandlesAndUrls(self, currentHandles=None):
-        if currentHandles is None:
-            currentHandles = self.__driver.window_handles
-        updated_handles = {}
+        try:
+            if currentHandles is None:
+                currentHandles = self.__driver.window_handles
 
-        for handle in currentHandles:
-            self.__driver.switch_to.window(handle)
-            url = self.__driver.current_url
-            updated_handles[handle] = url
+            currentHandlesSet = set(currentHandles)
+            existingHandlesSet = set(self.__handles.keys())
 
-        self.__handles = updated_handles
-        if self.__driver.current_window_handle in currentHandles:
-            self.__driver.switch_to.window(self.__driver.current_window_handle)
+            closedHandles = existingHandlesSet - currentHandlesSet
+            newHandles = currentHandlesSet - existingHandlesSet
 
-        self._handleCount = len(self.__handles)
+            for handle in closedHandles:
+                self.__deleteClosedTabHtmlContent(handle)
+                del self.__handles[handle]     
 
-        
+            for handle in newHandles:
+                self.__driver.switch_to.window(handle)
+                url = self.__driver.current_url
+                self.__handles[handle] = url
+
+            if self.__driver.current_window_handle in currentHandles:
+                self.__driver.switch_to.window(self.__driver.current_window_handle)
+
+            self._handleCount = len(self.__handles)
+        except NoSuchWindowException:
+            self.__handleWindowClosedScenario()
+            
+    def __handleWindowClosedScenario(self):
+        currentHandles = self.__driver.window_handles
+        if currentHandles:
+            self.__driver.switch_to.window(currentHandles[0])
+            self.__lastActiveHandle = currentHandles[0]
+            Logger.warn("Switched to a remaining window after the current window was closed.")
+        else:
+            Logger.error("No remaining windows to switch to. Program is stopping.")
+            self.__stopProgram()
+
+    def __stopProgram(self):
+        self.__driver.quit()
+        Logger.info("Program has been successfully stopped.")
+
+
     def __trackHtmlContentsOfUrls(self):
-        handles_dict = dict(self.__handles)
-        self.__contentFetcher.fetchAndPrintHtmlContents(handles_dict)
+        handlesDict = dict(self.__handles)
+        currentHandle = self.__driver.current_window_handle
+        self.__contentFetcher.fetchAndPrintHtmlContents(handlesDict, currentHandle)
         
-    #def __getCurrentUrls(self):
-    #    return list(self.__handles.values())
-    #
-    #def __printCurrentTabsAndUrls(self):
-    #    print("Aktif Sekmeler ve URL'leri:")
-    #    for handle, url in self.__handles.items():
-    #        print(f"Sekme: {handle}, URL: {url}")
-
-    #def __process(self) -> None:
-    #    self.__refreshThread.start()
-    #    self.__refreshThread.join()
-
-    #def start(self) -> None:
-    #    self.__thread.start()
-
-    #def join(self) -> None:
-    #    self.__thread.join()
-    #
-    #def __handleRefreshs(self) -> None:
-    #    while self.__handleCount != 0:
-    #        sleep(0.1)
-    
-    '''
-        def __injectEventListeners(self):
-        # Sadece click ve submit olaylarını yakalayacak ve localStorage'a kaydedecek JavaScript kodunu enjekte eder
-        js_code = """
-        document.body.addEventListener('click', function() {
-            localStorage.setItem('lastInteraction', new Date().getTime().toString());
-        }, true);
-        document.body.addEventListener('submit', function() {
-            localStorage.setItem('lastInteraction', new Date().getTime().toString());
-        }, true);
-        document.body.addEventListener('click', function() {
-        var now = new Date();
-        var day = now.getDate();
-        var month = now.getMonth() + 1; // JavaScript months are 0-based.
-        var year = now.getFullYear();
-        var hours = now.getHours();
-        var minutes = now.getMinutes();
-        var seconds = now.getSeconds();
-        
-        // Format the date and time components to ensure they are in two digits, except the year.
-        var formattedDate = day.toString().padStart(2, '0') + '-' + 
-                            month.toString().padStart(2, '0') + '-' + 
-                            year.toString();
-        var formattedTime = hours.toString().padStart(2, '0') + ':' + 
-                            minutes.toString().padStart(2, '0') + ':' + 
-                            seconds.toString().padStart(2, '0');
-        
-        // Combine the date and time in your preferred format.
-        var dateTimeString = formattedDate + ' ' + formattedTime;
-        
-        localStorage.setItem('lastInteraction', dateTimeString);
-        }, true);
-        """
-        self.__driver.execute_script(js_code)
-                        
-
-                        
-                                       try:
-                    last_interaction = self.__driver.execute_script("""
-        document.body.addEventListener('submit', function() {
-            localStorage.setItem('lastInteraction', new Date().getTime().toString());
-        }, true);
-        document.body.addEventListener('click', function() {
-            var now = new Date();
-            var day = now.getDate();
-            var month = now.getMonth() + 1; // JavaScript months are 0-based.
-            var year = now.getFullYear();
-            var hours = now.getHours();
-            var minutes = now.getMinutes();
-            var seconds = now.getSeconds();
-            
-            // Format the date and time components to ensure they are in two digits, except the year.
-            var formattedDate = day.toString().padStart(2, '0') + '-' + 
-                                month.toString().padStart(2, '0') + '-' + 
-                                year.toString();
-            var formattedTime = hours.toString().padStart(2, '0') + ':' + 
-                                minutes.toString().padStart(2, '0') + ':' + 
-                                seconds.toString().padStart(2, '0');
-            
-            // Combine the date and time in your preferred format.
-            var dateTimeString = formattedDate + ' ' + formattedTime;
-            
-            localStorage.setItem('lastInteraction', dateTimeString);
-        }, true);
-        return localStorage.getItem('lastInteraction');""")
-                    # Process last_interaction as needed
-                    #if last_interaction:
-                    #    print(last_interaction)
-                    #print(f"last_interaction: {last_interaction}\n")
-                except selenium.common.exceptions.JavascriptException as e:
-                    pass
-                    #Logger.error(f"JavaScript execution failed: {e}")
-                    # Handle the exception, e.g., by logging or taking alternative actions
-        '''
+    def __deleteClosedTabHtmlContent(self, deletedTab):
+        self.__contentFetcher.deleteFiles(deletedTab)
